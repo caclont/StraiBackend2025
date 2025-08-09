756d05c6c94b6d7f63320c55a3f03d1503ec5eb3
gambal <agambarini.info@gmail.com>
2025-08-09 16:26:05 +0200

    Add MyLife with Years

--- src/api/annee/controllers/annee.ts
+++
@@ -1,7 +0,0 @@
-/**
- * annee controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::annee.annee');
--- src/api/annee/services/annee.ts
+++
@@ -1,7 +0,0 @@
-/**
- * annee service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::annee.annee');
